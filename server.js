44a5045a59aa092af75cdfc1c7f72d7864de6ea1
Cody Doan <Cody@codys-mbp.attlocal.net>
2017-04-15 22:32:17 -0700

    setting server

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use("/styles",  express.static(path.join(__dirname + '/styles')));
+app.use("/scripts", express.static(__dirname + '/scripts'));
+
 app.set('port', (process.env.PORT || 8000))
 
 app.listen(app.get('port'), () => {
